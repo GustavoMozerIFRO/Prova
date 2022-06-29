d6931ec0d528b39212356c1fe5814f3c8b3cc2a2
GustavoMozerIFRO <gustavosmozer@gmail.com>
2022-06-29 09:06:31 -0400

    limpeza de trabalho

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      <p className='mozer'>
+        prova
+      </p>
+    </div>
+  )
+}
+
+export default App
